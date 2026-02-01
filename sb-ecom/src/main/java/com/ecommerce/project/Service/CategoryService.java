ea709d96b8f0f7147c550dedb759ebb36e5e79e8
rocketpub595 <kazisaifyasin@gmail.com>
2026-02-02 00:02:43 +0530

    Implemented and Utilized pagination metadata at service level for getAllCategories function

--- sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryService.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryService.java
@@ -2,9 +2,6 @@ package com.ecommerce.project.Service;
 import com.ecommerce.project.Payload.CategoryDTO;
 import com.ecommerce.project.Payload.CategoryResponse;
 
-import java.util.List;
-
-
 public interface CategoryService {
     CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
--- sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
@@ -41,6 +41,10 @@ public class CategoryServiceImpl implements CategoryService{
 
         CategoryResponse categoryResponse = new CategoryResponse();
         categoryResponse.setContent(categoryDTOS);
+        categoryResponse.setPageNumber(categoryPage.getNumber());
+        categoryResponse.setPageSize(categoryPage.getSize());
+        categoryResponse.setTotalElements(categoryPage.getTotalElements());
+        categoryResponse.setLastPage(categoryPage.isLast());
         return categoryResponse;
     }
 
