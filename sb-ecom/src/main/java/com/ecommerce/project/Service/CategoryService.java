2be42d7154930ec336af41946e31fe02a9fa34e8
rocketpub595 <kazisaifyasin@gmail.com>
2026-02-01 23:37:07 +0530

    Pagination Logic Implemented at Service Layer. Accepted PageNumber and PageSize. Utilizing Pageable Interface and PageRequest.of() implementation.

--- sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryService.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 
 public interface CategoryService {
-    CategoryResponse getAllCategories();
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
     //String  updateCategory(Long categoryId , String categoryName);
 
--- sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Service/CategoryServiceImpl.java
@@ -8,6 +8,9 @@ import com.ecommerce.project.Payload.CategoryResponse;
 import com.ecommerce.project.Repositories.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -22,8 +25,12 @@ public class CategoryServiceImpl implements CategoryService{
     private ModelMapper modelMapper;
 
     @Override
-    public CategoryResponse getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
+
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize);
+        Page<Category> categoryPage = categoryRepository.findAll(pageDetails);
+
+        List<Category> categories = categoryPage.getContent();
         if(categories.isEmpty()){
             throw new APIExceptions("Category Empty. Please add at least one category");
         }
