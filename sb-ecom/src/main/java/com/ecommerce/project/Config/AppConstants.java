eb7de7efdc4e642ea0e3e1d6893459d4702f67f8
rocketpub595 <kazisaifyasin@gmail.com>
2026-02-04 23:58:25 +0530

    Application Constants chagned according to needs

--- sb-ecom/src/main/java/com/ecommerce/project/Config/AppConstants.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Config/AppConstants.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.Config;
 
-public class AppConstants {
+public class AppConstants
+{
     public static final String PAGE_NUMBER = "0";
-    public static final String PAGE_SIZE = "10";
+    public static final String PAGE_SIZE = "15";
 }
