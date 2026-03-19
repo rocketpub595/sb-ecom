157219b08588409af6abf82aef05fd2b464bbc72
SK <kazisaifyasin@gmail.com>
2026-03-19 20:46:36 +0530

    Saving Product to Category Functions Defined at Service Layer

--- sb-ecom/src/main/java/com/ecommerce/project/Service/ProductService.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Service/ProductService.java
@@ -1,4 +1,8 @@
 package com.ecommerce.project.Service;
 
+import com.ecommerce.project.Model.Product;
+import com.ecommerce.project.Payload.ProductDTO;
+
 public interface ProductService {
+    ProductDTO addProduct(Long categoryId, Product product);
 }
--- sb-ecom/src/main/java/com/ecommerce/project/Service/ProductServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Service/ProductServiceImpl.java
@@ -1,7 +1,13 @@
 package com.ecommerce.project.Service;
 
+import com.ecommerce.project.Model.Product;
+import com.ecommerce.project.Payload.ProductDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        return null;
+    }
 }
