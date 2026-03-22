ef2ee617febbbd76c2d6d9bce81859ad5d67ffb4
SK <kazisaifyasin@gmail.com>
2026-03-22 20:20:20 +0530

    One-to-Many relation established between Category and Product

--- sb-ecom/src/main/java/com/ecommerce/project/Model/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Model/Category.java
@@ -1,12 +1,11 @@
 package com.ecommerce.project.Model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity(name = "Categories")
 @Data
@@ -20,4 +19,6 @@ public class Category {
     @Size(min = 5 , message = "Category name must contain 5 Characters")
     private String categoryName;
 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
 }
