e5f1790c2fdab0f89a11bd88a91b5ece203aa1fa
SK <kazisaifyasin@gmail.com>
2026-04-06 20:50:29 +0530

    Implement Users

--- sb-ecom/src/main/java/com/ecommerce/project/Model/Role.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Model/Role.java
@@ -1,9 +1,25 @@
 package com.ecommerce.project.Model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Data
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Integer roleId;
-    private String roleName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20 , name = "role_name")
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
 }
--- sb-ecom/src/main/java/com/ecommerce/project/Model/User.java
+++ sb-ecom/src/main/java/com/ecommerce/project/Model/User.java
@@ -1,7 +1,35 @@
 package com.ecommerce.project.Model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(min = 1, max = 20)
+    private String userName;
+    @NotBlank
+    @Size(min = 1, max = 50)
+    @Email
+    private String email;
+    @NotBlank
+    @Size(min = 1, max = 120)
+    private String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
 }
